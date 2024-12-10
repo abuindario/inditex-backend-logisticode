b2eb0163e6edf41713eb5fa5566605dfa89bb9c4
abuindario <darioabuinpose@gmail.com>
2024-12-10 21:00:22 +0100

    commenting CenterServiceController methods to know which needs to be fixed

--- src/main/java/com/hackathon/inditex/Controllers/OrderServiceController.java
+++ src/main/java/com/hackathon/inditex/Controllers/OrderServiceController.java
@@ -13,32 +13,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hackathon.inditex.DTO.Mapper;
 import com.hackathon.inditex.DTO.OrderDTO;
-import com.hackathon.inditex.DTO.ResponseMessage;
+import com.hackathon.inditex.DTO.ResponseOrderMessage;
 import com.hackathon.inditex.Entities.Order;
 import com.hackathon.inditex.Services.OrderServiceImpl;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderServiceController {
-	@Autowired OrderServiceImpl orderServiceImpl;
-	
+	@Autowired
+	OrderServiceImpl orderServiceImpl;
+
 	public Mapper mapper = new Mapper();
 
 	@GetMapping("")
 	public ResponseEntity<List<Order>> readOrders() {
 		return new ResponseEntity<>(orderServiceImpl.findAll(), HttpStatus.OK);
 	}
-	
-//	@PostMapping("") 
-//	public ResponseEntity<?> createNewOrder(@RequestBody OrderDTO orderDTO) {
-//		Order order = mapper.toOrder(orderDTO);
-//		if(order.getSize().length() == 1 && ( (order.getSize().toUpperCase().charAt(0) == 'B') || (order.getSize().charAt(0) == 'M') 
-//				|| (order.getSize().charAt(0) == 'S' ))) {
-//			order.setStatus("PENDING");
-//			orderServiceImpl.save(order);
-//			return new ResponseEntity<>(mapper.toResponseOrderMessage(order), HttpStatus.CREATED);
-//		} else {
-//			return new ResponseEntity<>(new ResponseMessage("Cannot create order."), HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//	}
+
+	@PostMapping("")
+	public ResponseEntity<ResponseOrderMessage> createNewOrder(@RequestBody OrderDTO orderDTO) {
+		Order order = mapper.toOrder(orderDTO);
+		if (order.getSize().length() <= 1
+				&& (order.getSize().length() == 0 || ((order.getSize().toUpperCase().charAt(0) == 'B')
+						|| (order.getSize().charAt(0) == 'M') || (order.getSize().charAt(0) == 'S')))) {
+			order.setStatus("PENDING");
+			orderServiceImpl.save(order);
+			return new ResponseEntity<>(mapper.toResponseOrderMessage(order), HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>(mapper.toResponseOrderMessage(order), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
