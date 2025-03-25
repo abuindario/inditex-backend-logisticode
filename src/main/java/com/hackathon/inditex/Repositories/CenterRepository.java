e2cd5f2998f34805fc29e82430284e55b1d575f2
abuindario <darioabuinpose@gmail.com>
2025-03-25 15:50:16 +0100

    set Transactional and Repository annotations for repositories

--- src/main/java/com/hackathon/inditex/Repositories/CenterRepository.java
+++ src/main/java/com/hackathon/inditex/Repositories/CenterRepository.java
@@ -1,9 +1,14 @@
 package com.hackathon.inditex.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hackathon.inditex.Entities.Center;
 
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface CenterRepository extends CrudRepository<Center, Long> {
 
 }
--- src/main/java/com/hackathon/inditex/Repositories/OrderRepository.java
+++ src/main/java/com/hackathon/inditex/Repositories/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.hackathon.inditex.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hackathon.inditex.Entities.Order;
 
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
 }
