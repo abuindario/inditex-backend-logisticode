0d61e8595ec76dc3c1f9f610fb0cde460c777c17
abuindario <darioabuinpose@gmail.com>
2025-03-27 09:10:33 +0100

    refactor repository

--- src/main/java/com/hackathon/inditex/Repositories/OrderRepository.java
+++ src/main/java/com/hackathon/inditex/Repositories/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.hackathon.inditex.Repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hackathon.inditex.Entities.Order;
 
 @Repository
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
